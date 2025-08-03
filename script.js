4c7cdf345761a3d899b3a68a0bca0e1c55bf8d15
Baibhab Adhikari <jiniaadhikari.resto@gmail.com>
2025-08-03 12:34:35 +0530

    Update AOS configuration to trigger animations only once during scrolling

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ document.addEventListener("DOMContentLoaded", function () {
   AOS.init({
     duration: 800, // values from 0 to 3000, with step 50ms
     easing: "ease-in-out", // default easing for AOS animations
-    once: false, // whether animation should happen only once - while scrolling down
+    once: true, // whether animation should happen only once - while scrolling down
     mirror: true, // whether elements should animate out while scrolling past them
   });
 
